aa05e69008dc9c03db58a2248aaf5d509952995c
Luís Egídio <luis.egidio@gmail.com>
2015-12-21 15:37:53 +0000

    Mark sheet snapshot domain

---
+++ src/main/java/org/fenixedu/ulisboa/specifications/domain/evaluation/markSheet/CompetenceCourseMarkSheetSnapshot.java
@@ -0,0 +1,9 @@
+package org.fenixedu.ulisboa.specifications.domain.evaluation.markSheet;
+
+public class CompetenceCourseMarkSheetSnapshot extends CompetenceCourseMarkSheetSnapshot_Base {
+    
+    public CompetenceCourseMarkSheetSnapshot() {
+        super();
+    }
+    
+}
---
+++ src/main/java/org/fenixedu/ulisboa/specifications/domain/evaluation/markSheet/CompetenceCourseMarkSheetSnapshotEntry.java
@@ -0,0 +1,9 @@
+package org.fenixedu.ulisboa.specifications.domain.evaluation.markSheet;
+
+public class CompetenceCourseMarkSheetSnapshotEntry extends CompetenceCourseMarkSheetSnapshotEntry_Base {
+    
+    public CompetenceCourseMarkSheetSnapshotEntry() {
+        super();
+    }
+    
+}
